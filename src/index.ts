a3bd7ab0a7a68ddddc56c710a377313487591e08
Dominik Koch <dominik@koch-bautechnik.de>
2024-07-23 16:54:01 +0200

    fix: make whole check better

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,48 @@
 import { codes } from "./codes";
 
 export function getDescription(code: number): string {
-  if (!code || typeof code !== number) {
-    throw new Error(`Expected code to be a number instead got ${typeof code}`);
+  if (!code || !Number.isInteger(code)) {
+    throw new TypeError(`Expected code to be an integer, instead got ${code}`);
+  }
+
+  if (code < 100 || code > 599) {
+    throw new RangeError(`Expected code to be between 100 and 599, instead got ${code}`);
   }
 
   return codes[code.toString()] || "Unknown Status Code";
 }
 
 export function isSuccess(code: number): boolean {
-  if (!code || typeof code !== number) {
-    throw new Error(`Expected code to be a number instead got ${typeof code}`);
+  if (!code || !Number.isInteger(code)) {
+    throw new TypeError(`Expected code to be an integer, instead got ${code}`);
+  }
+
+  if (code < 100 || code > 599) {
+    throw new RangeError(`Expected code to be between 100 and 599, instead got ${code}`);
   }
 
   return code >= 200 && code < 300;
 }
 
 export function isError(code: number): boolean {
-  if (!code || typeof code !== number) {
-    throw new Error(`Expected code to be a number instead got ${typeof code}`);
+  if (!code || !Number.isInteger(code)) {
+    throw new TypeError(`Expected code to be an integer, instead got ${code}`);
+  }
+
+  if (code < 100 || code > 599) {
+    throw new RangeError(`Expected code to be between 100 and 599, instead got ${code}`);
   }
 
   return code >= 400 && code < 600;
 }
 
 export function handleHttp(code: number) {
-  if (!code || typeof code !== number) {
-    throw new Error(`Expected code to be a number instead got ${typeof code}`);
+  if (!code || !Number.isInteger(code)) {
+    throw new TypeError(`Expected code to be an integer, instead got ${code}`);
+  }
+
+  if (code < 100 || code > 599) {
+    throw new RangeError(`Expected code to be between 100 and 599, instead got ${code}`);
   }
 
   return {
