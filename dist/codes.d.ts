0b72be30fc40242b0c97cf938c7018add50394d4
dpaulos6 <dpaulos6@users.noreply.github.com>
2025-01-02 09:59:25 +0000

    chore(build): update build files

--- dist/codes.d.ts
+++ dist/codes.d.ts
@@ -1,5 +1,12 @@
+/**
+ * Interface representing a collection of HTTP status codes.
+ */
 interface Code {
     [code: string]: string;
 }
+/**
+ * A collection of HTTP status codes and their corresponding messages.
+ * @type {Code}
+ */
 export declare const codes: Code;
 export {};
